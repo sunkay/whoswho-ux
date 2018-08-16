39cfd41dcaecf9bf79bca7f9ac1b625059fa9212
sunkay <sunkay11@gmail.com>
2018-08-16 07:49:57 -0700

    material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ApolloClient from "apollo-boost";
+import 'typeface-roboto';
 import logo from './logo.svg';
 import './App.css';
 import './EmpList';
 
 import { ApolloProvider } from "react-apollo";
 import EmpList from './EmpList';
+import Dashboard from './Dashboard';
 
 const client = new ApolloClient({
   uri: "http://localhost:4000"
@@ -17,18 +19,10 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>My first Apollo app 
-            <span role="img" aria-label="rocket">🚀</span>
-            </h2>
-          </header>
           <div>
+            <Dashboard />
             <EmpList />
           </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
         </div>
       </ApolloProvider>
     );
