c60cfa338cfbce800546579ab47d879382df7eaf
sunkay <sunkay11@gmail.com>
2018-08-21 08:18:31 -0700

    downshift material-ui

--- src/ApolloAutoComplete.js
+++ src/ApolloAutoComplete.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import Downshift from "downshift";
-
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
 import { Query } from "react-apollo";
+import { MenuItem } from "@material-ui/core";
 
 const styles = theme => ({
   root: {
@@ -32,7 +35,7 @@ const styles = theme => ({
 });
 
 const GET_EMP = gql`
-  query AllEmployees($inputValue: String!){
+  query AllEmployees($inputValue: String!) {
     allEmployees(filter: $inputValue) {
       id
       firstname
@@ -41,7 +44,9 @@ const GET_EMP = gql`
   }
 `;
 
-function ApolloAutocomplete() {
+function ApolloAutocomplete(props) {
+  const { classes } = props;
+
   return (
     <Downshift onChange={selectedItem => alert(selectedItem)}>
       {({
@@ -51,10 +56,16 @@ function ApolloAutocomplete() {
         getItemProps,
         selectedItem,
         highlightedIndex,
-        isOpen,
+        isOpen
       }) => (
-        <div>
-          <input {...getInputProps()} />
+        <div className={classes.container}>
+          {renderInput({
+            fullWidth: true,
+            classes,
+            InputProps: getInputProps({
+              placeholder: "Search a country (start with a)"
+            })
+          })}
           <ApolloAutoCompleteMenu
             {...{
               inputValue,
@@ -63,12 +74,13 @@ function ApolloAutocomplete() {
               selectedItem,
               highlightedIndex,
               isOpen,
+              classes
             }}
           />
         </div>
       )}
     </Downshift>
-  )
+  );
 }
 
 function ApolloAutoCompleteMenu({
@@ -78,57 +90,77 @@ function ApolloAutoCompleteMenu({
   getItemProps,
   getMenuProps,
   inputValue,
-}){
-  if(!isOpen){
-    return null
+  classes
+}) {
+  if (!isOpen) {
+    return null;
   }
 
-  return(
+  return (
     <Query
       query={GET_EMP}
       variables={{
-        inputValue,
+        inputValue
       }}
     >
-    {({loading, error, data}) => {
-      console.log("data", data);
-      const allEmps = (data && data.allEmployees) || []
+      {({ loading, error, data }) => {
+        console.log("data", data);
+        const allEmps = (data && data.allEmployees) || [];
 
-      if(loading){
-        return <div>loading...</div>
-      }
+        if (loading) {
+          return <div>loading...</div>;
+        }
 
-      if(error){
-        return <div>Error! ${error.message}</div>
-      }
+        if (error) {
+          return <div>Error! ${error.message}</div>;
+        }
 
-      return(
-        <ul
-            {...getMenuProps({
-              style: {padding: 0, margin: 0, listStyle: 'none'},
-            })}
-          >
-            {allEmps.map(({firstname: item}, index) => (
-              <li
-                key={item}
-                {...getItemProps({
-                  index,
-                  item,
-                  style: {
-                    backgroundColor:
-                      highlightedIndex === index ? 'lightgray' : 'white',
-                    fontWeight: selectedItem === item ? 'bold' : 'normal',
-                  },
-                })}
-              >
-                {item}
-              </li>
-            ))}
-          </ul>
-      )
-    }}
+        return (
+          <Paper className={classes.paper} square>
+            <ul
+              {...getMenuProps({
+                style: { padding: 0, margin: 0, listStyle: "none" }
+              })}
+            >
+              {allEmps.map(({ firstname: item }, index) => (
+                <MenuItem
+                  key={item}
+                  {...getItemProps({
+                    index,
+                    item,
+                    style: {
+                      backgroundColor:
+                        highlightedIndex === index ? "lightgray" : "white",
+                      fontWeight: selectedItem === item ? "bold" : "normal"
+                    }
+                  })}
+                >
+                  {item}
+                </MenuItem>
+              ))}
+            </ul>
+          </Paper>
+        );
+      }}
     </Query>
-  )
+  );
 }
 
-export default ApolloAutocomplete;
+function renderInput(inputProps) {
+  const { InputProps, classes, ref, ...other } = inputProps;
+
+  return (
+    <TextField
+      InputProps={{
+        inputRef: ref,
+        classes: {
+          root: classes.inputRoot
+        },
+        ...InputProps
+      }}
+      {...other}
+    />
+  );
+}
+
+export default withStyles(styles)(ApolloAutocomplete);
